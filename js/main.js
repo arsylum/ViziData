37e0fdab474f7230a1cd192ab1dbdf4cda3784a8
gordelwig <georg.wild@tu-dresden.de>
2015-06-30 03:48:24 +0200

    cleaning up

--- js/main.js
+++ js/main.js
@@ -6644,30 +6644,6 @@ function setSetSel(dsi, dgi) {
         genGrid();
     } else {
         // loading feedback
-        // var l = 0;
-        // var lAnim = setInterval(function(){
-        // var txt = [
-        // 	"loading... &nbsp; &nbsp; ┬──┬",
-        // 	"loading... &nbsp; &nbsp; ┬──┬",
-        // 	"loading... (°o°） ┬──┬",
-        // 	"loading... &nbsp;(°o°）┬──┬",
-        // 	"loading... (╯°□°）╯ ┻━┻",
-        // 	"loading... (╯°□°）╯︵ ┻━┻",
-        // 	"loading... (╯°□°）╯︵ ︵ ┻━┻",
-        // 	"loading... (╯°□°）╯︵ ︵ ︵ ┻━┻",
-        // 	"loading... ︵ ︵ ︵ ┻━┻",
-        // 	"loading... ︵ ︵ ┻━┻",
-        // 	"loading... ︵ ┻━┻",
-        // 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp; &nbsp; (ツ)",
-        // 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp; &nbsp;(ツ)",
-        // 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp; (ツ)",
-        // 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp;(ツ)",
-        // 	"loading... &nbsp; &nbsp; ┬──┬ ¯\\_(ツ)",
-        // 	"loading... &nbsp; &nbsp; ┬──┬ (ツ)",
-        // 	];
-        // $("#dsdesc").html(txt[l]);
-        // l = (l+1)%17;
-        // },180);
         $("#dsdesc").html("<h4>Dataset <em>" + current_datsel.datasets[dsi].strings.label + "</em></h4>" + '<div class="loading-box"><div class="loading-bar"></div></div>');
         var lBM = new Date();
         console.log("~~ starting to load dataset " + current_datsel.datasets[dsi].strings.label + " ~~ ");
--- js/modular/data.js
+++ js/modular/data.js
@@ -29,31 +29,6 @@ function setSetSel(dsi, dgi) { //, callback){
 		genGrid();
 	} else {
 		// loading feedback
-		// var l = 0;
-		// var lAnim = setInterval(function(){
-		// var txt = [
-		// 	"loading... &nbsp; &nbsp; ┬──┬﻿",
-		// 	"loading... &nbsp; &nbsp; ┬──┬﻿",
-		// 	"loading... (°o°） ┬──┬﻿",
-		// 	"loading... &nbsp;(°o°）┬──┬﻿",
-		// 	"loading... (╯°□°）╯ ┻━┻",
-		// 	"loading... (╯°□°）╯︵ ┻━┻",
-		// 	"loading... (╯°□°）╯︵ ︵ ┻━┻",
-		// 	"loading... (╯°□°）╯︵ ︵ ︵ ┻━┻",
-		// 	"loading... ︵ ︵ ︵ ┻━┻",
-		// 	"loading... ︵ ︵ ┻━┻",
-		// 	"loading... ︵ ┻━┻",
-		// 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp; &nbsp; (ツ)",
-		// 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp; &nbsp;(ツ)",
-		// 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp; (ツ)",
-		// 	"loading... &nbsp; &nbsp; ┻━┻ &nbsp;(ツ)",
-		// 	"loading... &nbsp; &nbsp; ┬──┬﻿ ¯\\_(ツ)",
-		// 	"loading... &nbsp; &nbsp; ┬──┬﻿ (ツ)",
-		// 	];
-		// $("#dsdesc").html(txt[l]);
-		// l = (l+1)%17;
-		// },180);
-
 		$("#dsdesc").html(
 			'<h4>Dataset <em>' + current_datsel.datasets[dsi].strings.label + '</em></h4>' +
 			'<div class="loading-box"><div class="loading-bar"></div></div>');
